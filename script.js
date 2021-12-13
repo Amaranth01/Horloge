d37f8bfe1f491fb09a06321be59cae0112084622
Amaranth01 <vanessa.nectoux@gmail.com>
2021-12-13 16:22:02 +0100

    Clock ok

--- script.js
+++ script.js
@@ -2,16 +2,16 @@ function date () {
     let hrsReel = document.querySelector('#hour');
     let minsReel = document.querySelector("#minute");
     let secondsReel = document.querySelector('#second');
-    const Today = new Date();
 
-    let seconds = Today.getSeconds();
-    secondsReel.style.transform = "rotate (" + (seconds *6) + " deg)";
+    let seconds = new Date().getSeconds();
+    secondsReel.style.transform = "rotate(" + (seconds *6) + "deg)";
 
-    let mins = Today.getMinutes();
-    minsReel.style.transform = "rotate (" + (mins * 6 ) + "deg)";
+    let mins = new Date().getMinutes();
+    minsReel.style.transform = "rotate(" + (mins * 6 ) + "deg)";
 
-    let hrs = Today.getHours()
-    hrsReel.style.transform = "rotate (" + (hrs * 30) + "deg)";
+    let hrs = new Date().getHours()
+    hrsReel.style.transform = "rotate(" + (hrs * 30) + "deg)";
+    console.log(seconds);
 }
 
 setInterval(date, 1000);
